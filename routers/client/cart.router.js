5c29353bda7a81834582b380eadb3ca200669256
giangka10052006 <giangka20061005@gmail.com>
2026-09-22 21:42:03 +0700

    Tích hợp trang giỏ hàng

---
+++ routers/client/cart.router.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+
+const cartController = require("../../controllers/client/cart.controller");
+
+router.get("/", cartController.cart);
+
+
+module.exports = router;
--- routers/client/index.router.js
+++ routers/client/index.router.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
 const homeRouters = require("./home.router");
 const tourRouters = require("./tour.router");
+const cartRouters = require("./cart.router");
 
 
 router.use("/", homeRouters);
 
 router.use('/tour',tourRouters);
+
+router.use("/cart", cartRouters);
+
 
 module.exports = router;
